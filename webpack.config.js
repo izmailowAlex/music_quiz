03ee2310915f052b1c8a530233deef70b283aee8
Aleks_bass <105873742+izmailowAlex@users.noreply.github.com>
2023-10-17 21:07:14 +0300

    feat: add favicon

--- webpack.config.js
+++ webpack.config.js
@@ -1,62 +1,64 @@
 /* eslint @typescript-eslint/no-var-requires: "off" */
-const path = require("path");
-const HTMLWebpackPlugin = require("html-webpack-plugin");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-  entry: {
-    main: path.resolve(__dirname, "./src/index.ts"),
-  },
-  output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "./dist"),
-  },
-  plugins: [
-    new HTMLWebpackPlugin({
-      title: "Music Quiz",
-      template: path.resolve(__dirname, "./src/index.html"),
-      filename: "index.html",
-    }),
-    new HTMLWebpackPlugin({
-      title: "Music Quiz",
-      template: path.resolve(__dirname, "./src/game.html"),
-      filename: "game.html",
-    }),
-    new CleanWebpackPlugin(),
-  ],
-  devServer: {
-    port: 8080,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.ts$/,
-        exclude: /node_modules/,
-        use: ["ts-loader"],
+ entry: {
+  main: path.resolve(__dirname, './src/index.ts'),
+ },
+ output: {
+  filename: 'bundle.js',
+  path: path.resolve(__dirname, './dist'),
+ },
+ plugins: [
+  new HTMLWebpackPlugin({
+   title: 'Music Quiz',
+   template: path.resolve(__dirname, './src/index.html'),
+   filename: 'index.html',
+   favicon: 'src/assets/img/favicon.ico',
+  }),
+  new HTMLWebpackPlugin({
+   title: 'Music Quiz',
+   template: path.resolve(__dirname, './src/game.html'),
+   filename: 'game.html',
+   favicon: 'src/assets/img/favicon.ico',
+  }),
+  new CleanWebpackPlugin(),
+ ],
+ devServer: {
+  port: 8080,
+ },
+ module: {
+  rules: [
+   {
+    test: /\.ts$/,
+    exclude: /node_modules/,
+    use: ['ts-loader'],
+   },
+   {
+    test: /\.(?:ico|gif|png|jpe?g)$/i,
+    type: 'asset/resource',
+   },
+   {
+    test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+    type: 'asset/inline',
+   },
+   {
+    test: /\.(mov|mp4)$/,
+    use: [
+     {
+      loader: 'file-loader',
+      options: {
+       name: '[name].[ext]',
       },
-      {
-        test: /\.(?:ico|gif|png|jpe?g)$/i,
-        type: "asset/resource",
-      },
-      {
-        test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
-        type: "asset/inline",
-      },
-      {
-        test: /\.(mov|mp4)$/,
-        use: [
-          {
-            loader: "file-loader",
-            options: {
-              name: "[name].[ext]",
-            },
-          },
-        ],
-      },
-      {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"],
-      },
+     },
     ],
-  },
+   },
+   {
+    test: /\.css$/,
+    use: ['style-loader', 'css-loader'],
+   },
+  ],
+ },
 };
