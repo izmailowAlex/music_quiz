02483b69155f82e571e9579eeacbbb39f2cf4aa2
Aleks_bass <105873742+izmailowAlex@users.noreply.github.com>
2023-10-17 17:22:48 +0300

    feat: implement start page & make some config settings

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,26 @@
 /* eslint @typescript-eslint/no-var-requires: "off" */
-const path = require('path');
-const HTMLWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const path = require("path");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
   entry: {
-    main: path.resolve(__dirname, './src/index.ts')
+    main: path.resolve(__dirname, "./src/index.ts"),
   },
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, './dist'),
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "./dist"),
   },
   plugins: [
     new HTMLWebpackPlugin({
-      title: 'Music Quiz',
-      template: path.resolve(__dirname, './src/index.html'),
-      filename: 'index.html',
+      title: "Music Quiz",
+      template: path.resolve(__dirname, "./src/index.html"),
+      filename: "index.html",
     }),
     new HTMLWebpackPlugin({
-      template: path.resolve(__dirname, './src/game.html'),
-      filename: 'game.html',
+      title: "Music Quiz",
+      template: path.resolve(__dirname, "./src/game.html"),
+      filename: "game.html",
     }),
     new CleanWebpackPlugin(),
   ],
@@ -28,23 +29,34 @@ module.exports = {
   },
   module: {
     rules: [
-        {
-          test: /\.ts$/,
-          exclude: /node_modules/,
-          use: ['ts-loader'],
-        },
-        {
-          test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
-          type: 'asset/resource',
-        },
-        {
-          test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
-          type: 'asset/inline',
-        },
-        {
-            test: /\.css$/,
-            use: ['style-loader', 'css-loader'],
-        },
+      {
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        use: ["ts-loader"],
+      },
+      {
+        test: /\.(?:ico|gif|png|jpe?g)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+        type: "asset/inline",
+      },
+      {
+        test: /\.(mov|mp4)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name].[ext]",
+            },
+          },
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
     ],
-  }
-}
+  },
+};
