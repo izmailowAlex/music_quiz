35bc5721f4795ffe8504bcbc707718416a8c4462
Aleks_bass <105873742+izmailowAlex@users.noreply.github.com>
2023-10-19 12:08:54 +0300

    refactor: change rules in .prettierrc & implement them for all files

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,19 @@
-import "./css/style.css";
-import video_view from "./assets/video/video_view.mp4";
+import './css/style.css';
+import video_view from './assets/video/video_view.mp4';
 console.log(video_view);
 
 function add(): void {
-  const VIDEO = document.createElement("video");
-  VIDEO.classList.add("video");
-  VIDEO.setAttribute("autoplay", "autoplay");
-  VIDEO.setAttribute("muted", "true");
-  VIDEO.setAttribute("loop", "true");
+  const VIDEO = document.createElement('video');
+  VIDEO.classList.add('video');
+  VIDEO.setAttribute('autoplay', 'autoplay');
+  VIDEO.setAttribute('muted', 'true');
+  VIDEO.setAttribute('loop', 'true');
 
-  const SOURCE = document.createElement("source");
-  SOURCE.setAttribute("src", `${video_view}`);
-  SOURCE.setAttribute("type", "video/mp4");
+  const SOURCE = document.createElement('source');
+  SOURCE.setAttribute('src', `${video_view}`);
+  SOURCE.setAttribute('type', 'video/mp4');
 
-  const MAIN = document.querySelector(".main");
+  const MAIN = document.querySelector('.main');
 
   VIDEO?.append(SOURCE);
   MAIN?.append(VIDEO);
--- webpack.config.js
+++ webpack.config.js
@@ -4,61 +4,61 @@ const HTMLWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
- entry: {
-  main: path.resolve(__dirname, './src/index.ts'),
- },
- output: {
-  filename: 'bundle.js',
-  path: path.resolve(__dirname, './dist'),
- },
- plugins: [
-  new HTMLWebpackPlugin({
-   title: 'Music Quiz',
-   template: path.resolve(__dirname, './src/index.html'),
-   filename: 'index.html',
-   favicon: 'src/assets/img/favicon.ico',
-  }),
-  new HTMLWebpackPlugin({
-   title: 'Music Quiz',
-   template: path.resolve(__dirname, './src/game.html'),
-   filename: 'game.html',
-   favicon: 'src/assets/img/favicon.ico',
-  }),
-  new CleanWebpackPlugin(),
- ],
- devServer: {
-  port: 8080,
- },
- module: {
-  rules: [
-   {
-    test: /\.ts$/,
-    exclude: /node_modules/,
-    use: ['ts-loader'],
-   },
-   {
-    test: /\.(?:ico|gif|png|jpe?g)$/i,
-    type: 'asset/resource',
-   },
-   {
-    test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
-    type: 'asset/inline',
-   },
-   {
-    test: /\.(mov|mp4)$/,
-    use: [
-     {
-      loader: 'file-loader',
-      options: {
-       name: '[name].[ext]',
-      },
-     },
-    ],
-   },
-   {
-    test: /\.css$/,
-    use: ['style-loader', 'css-loader'],
-   },
+  entry: {
+    main: path.resolve(__dirname, './src/index.ts'),
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, './dist'),
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      title: 'Music Quiz',
+      template: path.resolve(__dirname, './src/index.html'),
+      filename: 'index.html',
+      favicon: 'src/assets/img/favicon.ico',
+    }),
+    new HTMLWebpackPlugin({
+      title: 'Music Quiz',
+      template: path.resolve(__dirname, './src/game.html'),
+      filename: 'game.html',
+      favicon: 'src/assets/img/favicon.ico',
+    }),
+    new CleanWebpackPlugin(),
   ],
- },
+  devServer: {
+    port: 8080,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        use: ['ts-loader'],
+      },
+      {
+        test: /\.(?:ico|gif|png|jpe?g)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+        type: 'asset/inline',
+      },
+      {
+        test: /\.(mov|mp4)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+            },
+          },
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
 };
